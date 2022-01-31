5556d48cdbdae1d55cc974ae1a47d8fbf6c6e55e
Lester Huang <huangl302d@gmail.com>
2022-01-30 19:07:06 -0500

    working toggles, next step: edit mode

--- client/src/components/Cards/StandardCard.vue
+++ client/src/components/Cards/StandardCard.vue
@@ -1,5 +1,6 @@
 <template>
-	<div class="card">
+	<!-- Normal showcase card -->
+	<div v-if="!editMode" class="card">
 		<div class="card-basic">
 			<div class="left">
 				<p class="company">Millennium</p>
@@ -7,10 +8,10 @@
 			</div>
 			<div class="separator"></div>
 			<div class="right">
-				<q-chip class="status">Accepted</q-chip>
+				<q-chip class="status" label="Accepted"></q-chip>
 				<div class="actions">
 					<!-- Default checkbox is unchecked -->
-					<label class="checkbox">
+					<!-- <label class="checkbox">
 						<input type="checkbox" v-model="editMode" />
 						<i class="fas fa-pen unchecked"></i>
 						<i class="fas fa-trash-alt checked"></i>
@@ -21,7 +22,59 @@
 						<input type="checkbox" v-model="detailMode" />
 						<i class="fas fa-chevron-down unchecked"></i>
 						<i class="fas fa-chevron-up checked"></i>
-					</label>
+					</label> -->
+
+					<q-btn
+						flat
+						round
+						icon="fas fa-pen"
+						size="sm"
+						@click="editMode = true"
+					></q-btn>
+					<q-btn
+						ref="detailToggleBtn"
+						flat
+						round
+						:icon="detailIcon"
+						size="sm"
+						@click="toggleDetail()"
+					></q-btn>
+				</div>
+			</div>
+		</div>
+
+		<DetailCard v-if="detailMode"></DetailCard>
+	</div>
+
+	<!-- Edit Card -->
+	<div v-if="editMode" class="card">
+		<div class="card-basic">
+			<div class="left">
+				<p class="company">Millennium</p>
+				<p class="position">QA Intern</p>
+			</div>
+			<div class="separator"></div>
+			<div class="right">
+				<q-chip class="status">Accepted</q-chip>
+				<div class="actions">
+					<q-btn flat round icon="fas fa-trash-alt" size="sm"></q-btn>
+					<q-btn
+						flat
+						round
+						icon="fas fa-times"
+						size="sm"
+						@click="editMode = false"
+					></q-btn>
+					<q-btn flat round icon="fas fa-check" size="sm"></q-btn>
+
+					<q-btn
+						ref="detailToggleBtn"
+						flat
+						round
+						:icon="detailIcon"
+						size="sm"
+						@click="toggleDetail()"
+					></q-btn>
 				</div>
 			</div>
 		</div>
@@ -33,18 +86,34 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import DetailCard from "./DetailCard.vue";
+import EditCard from "./EditCard.vue";
 
 export default defineComponent({
+	components: { DetailCard, EditCard },
 	setup() {
+		// Edit mode vars
 		const editMode = ref(false);
+
+		// Detail mode vars
 		const detailMode = ref(false);
+		const detailIcon = "fas fa-chevron-down";
+		const detailAltIcon = "fas fa-chevron-up";
 
 		return {
 			editMode,
 			detailMode,
+			detailIcon,
+			detailAltIcon,
 		};
 	},
-	components: { DetailCard },
+	methods: {
+		toggleDetail() {
+			this.detailMode = !this.detailMode;
+			const tmp = this.detailAltIcon;
+			this.detailAltIcon = this.detailIcon;
+			this.detailIcon = tmp;
+		},
+	},
 });
 </script>
 
@@ -70,17 +139,23 @@ export default defineComponent({
 
 	.right {
 		flex-shrink: 1;
+		flex-wrap: wrap;
 
 		.status {
 			// Makes it inline with the company
-			margin-inline: 0;
-			margin-top: 12px;
-			margin-bottom: 0;
+			margin-top: 4px;
+			margin-bottom: 4px;
+			margin-left: 35px;
 			font-size: 1rem;
+			flex-grow: 0;
 
 			@media (max-width: $breakpoint-sm) {
 				margin: 0;
 				margin-top: 5px;
+			}
+
+			@media (max-width: 320px) {
+				margin-right: auto;
 			}
 		}
 
@@ -90,31 +165,16 @@ export default defineComponent({
 			margin-right: 4px;
 			justify-content: flex-end;
 			font-size: 16px;
-			column-gap: 8px;
+			column-gap: 2px;
 
 			@media (max-width: $breakpoint-sm) {
 				align-items: center;
 				margin-top: 0;
 			}
 
-			label {
-				display: flex;
-				column-gap: 8px;
-			}
-
-			.checkbox {
-				input[type="checkbox"],
-				.checked {
-					display: none;
-				}
-
-				input[type="checkbox"]:checked ~ .checked {
-					display: inline;
-				}
-
-				input[type="checkbox"]:checked ~ .unchecked {
-					display: none;
-				}
+			@media (max-width: 320px) {
+				justify-content: flex-start;
+				margin-top: 10px;
 			}
 		}
 
@@ -122,6 +182,12 @@ export default defineComponent({
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
+		}
+
+		@media (max-width: 320px) {
+			flex-direction: column;
+			justify-content: flex-start;
+			margin-top: 0;
 		}
 	}
 
